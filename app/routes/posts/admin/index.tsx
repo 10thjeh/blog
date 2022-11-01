fb76d6bc4b453af87bf9fc3e48d0c30b8accb3ac
jesse-ev <Jesse.Evans@hashmicro.com>
2022-11-01 11:29:01 +0700

    initial commit

---
+++ app/routes/posts/admin/index.tsx
@@ -0,0 +1,9 @@
+import { Link } from "@remix-run/react";
+
+export default function AdminIndex() {
+  return (
+    <div>
+      <h1>Please choose one post to be displayed here</h1>
+    </div>
+  );
+}
